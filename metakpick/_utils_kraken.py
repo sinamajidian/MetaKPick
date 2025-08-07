361943b7ae1c4028af74b4010e368808cb66279f
smajidi5 <smajidi5@devlangmead1.cm.cluster>
2025-08-06 20:39:48 -0400

    redo

--- metakpick/_utils_kraken.py
+++ metakpick/_utils_kraken.py
@@ -1,64 +1,50 @@
 
+import os
+import pandas as pd
 
-def read_kraken_set(file,read_names):
-
-    # "C"/"U": a one letter code indicating that the sequence was either classified or unclassified.
-    # The sequence ID, obtained from the FASTA/FASTQ header.
-    # The taxonomy ID Kraken 2 used to label the sequence; this is 0 if the sequence is unclassified.
-    # The length of the sequence in bp. In the case of paired read data, this will be a string containing the lengths of the two sequences in bp, separated by a pipe character, e.g. "98|94".
-    # A space-delimited list indicating the LCA mapping of each  k-mer in the sequence(s).
-    # For example, "562:13 561:4 A:31 0:1 562:3" would indicate that:
-    # the first 13 k-mers mapped to taxonomy ID #562 ... 31  ambiguous nucleotide (A)
-    file_o=open(file,'r')
-    dic_krak={}
-    for line in file_o:
-        line_split=line.strip().split("\t")
-        if line_split[0]=="C":
-            read_id, tax_krak, read_len, tax_kmers = line_split[1:]
-            if read_id in read_names:
-                tax_kmer_dic={}
-                tax_kmer_num_dic={}
-                pos=-1
-                for tax_kmer in tax_kmers.split(" "):
-                    
-                    tax_, num_raw =  tax_kmer.split(":")
-                    
-                    num=int(num_raw)
-                    pos+=num     # how many consecutive kmer has this taxid # 0:6 543:1 91347:1 six unclassified, 
-                    if tax_=='0' or tax_ =='A':
-                        continue
-
-                    tax=int(tax_)
-                    if tax in tax_kmer_dic:
-                        tax_kmer_dic[tax].append( (pos,num))
-                        tax_kmer_num_dic[tax]+=num
-                    else:    
-                        tax_kmer_dic[tax]=[(pos,num)]
-                        tax_kmer_num_dic[tax]=num
-                dic_krak[read_id] = int(tax_krak), int(read_len), tax_kmer_dic, tax_kmer_num_dic
-    return dic_krak
-
-
-
-def read_kraken_set(folder, cases, readids_max):
-    print("read kraken's k-mer  count per tax")
-    #folder="/vast/blangme2/smajidi5/metagenomics/changek/simulatation/classification/" # 
-    #cases=['k19','k25','k31'] # ,'k21'
-    dic_cases={}
-    for case in reversed(cases): # 
-        print(case)
-        try:
-            #dic_cases[case]=read_kraken_limited(folder+case+"_out",10000)
-            #dic_cases[case]=read_kraken(folder+case+"_out")
-            dic_cases[case]=read_kraken_set(folder+case+"_out",readids_max)
-        except:
-            print("n",case)
-        print(case,len(dic_cases[case]))
-
-    cases_readids=set()
-    for case_k, case in  enumerate(cases): 
-        read_ids_k=set(dic_cases[case].keys())
-        cases_readids |= read_ids_k
-    len(cases_readids)
-    return dic_cases, cases_readids
+def read_kraken_classification(cases , truth_file, classification_folder):
     
+    #cases=cases[:-1]; for k in range(20,26): cases.append("k"+str(k)+"l15")
+    print(len(cases),cases)
+    tool_res_files=[]
+    print(classification_folder)
+    for case in cases: 
+        f=classification_folder+case+"_out.csv"
+        tool_res_files.append(f)
+    len(tool_res_files)
+    merged = pd.read_csv(truth_file, names=["read_name", "taxon"])
+    for f in tool_res_files:
+        c=f.split("/")[-1][1:3]
+        print(c)
+        merged_ = pd.read_csv(f, names=["read_name", "taxon_tool_k"+c],sep=",")
+        print(f, len(merged_))
+        merged = pd.merge(merged, merged_, on="read_name")
+    merged.taxon = merged.taxon.astype(int)
+    # merging only gets the intersection of all (which is fine, files should have all)
+
+    column_names = list(merged.columns)
+    print(len(merged),column_names )
+    return merged 
+
+
+def get_tax_depth(merged, cases, info,parents):
+    read_tax_depth={}
+    for case in cases: 
+        read_tax_depth[case]={}
+        tocheck_=""
+        for index, row in merged.iterrows():
+            taxid=row['taxon_tool_'+case]
+            tax2root_list = find_tax2root(info,parents, taxid) # [2711156, 1649486, 41297, 204457, 28211, 1224, 3379134, 2, 131567, 1]
+            if tax2root_list!=-1:
+                depth=len(tax2root_list)
+            else:
+                depth=0
+            if depth<3:
+                tocheck_=taxid
+            read_tax_depth[case][row['read_name']]=depth
+            
+    print(tocheck_)
+    print(len(read_tax_depth),len(read_tax_depth[case]))
+    print(Counter(read_tax_depth[case].values()))
+
+    return read_tax_depth   
--- metakpick/_utils_tree.py
+++ metakpick/_utils_tree.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 def parse_tree_file(tree_df,children,parents):
     # create a dictionary from taxonomic id to index of corresponding row
@@ -15,19 +16,6 @@ def parse_tree_file(tree_df,children,parents):
         else:
             Tree[parent] = {child}
     return info, Tree 
-    
-def find_tax_level(info,tree_df,parents, tid, tax_level="species"):
-    if tid not in info:
-        #print(tid)
-        return -1
-    while tid != 1:    
-        tid_row = info[tid]
-        if tree_df[4][tid_row] == tax_level:
-            break
-        else:
-            tid = parents[info[tid]] #find_parent_node(tid)
-            # if retrun 1, it means that the tid is more towaards root that the requested level
-    return tid
 
 
 def find_tax2root(info,parents, tid):
@@ -43,54 +31,16 @@ def find_tax2root(info,parents, tid):
 
 
 
-def lca_finder_list(info,parents, tax_list):
-    lca=tax_list[0]
-    
-    for tax2 in tax_list[1:]:
-        lca = lca_finder(info,parents, lca,tax2)
-        
-    return lca
-
-def lca_finder(info,parents, tax1,tax2):
-    list_tax2root_1 = find_tax2root(info,parents, tax1)
-    list_tax2root_2 = find_tax2root(info,parents, tax2)
-    
-    list_tax2root1 =list_tax2root_1 [::-1] # reverse from root to leaf
-    list_tax2root2 =list_tax2root_2 [::-1]
-    
-    depth_i = 0
-    while(depth_i < len(list_tax2root1) and depth_i < len(list_tax2root2)):
-        #print(depth_i)
-        if list_tax2root1[depth_i] != list_tax2root2[depth_i]:
-            break
-        depth_i += 1
-    assert list_tax2root1[depth_i-1] == list_tax2root2[depth_i-1], "issue in LCA finding"
-    lca=list_tax2root1[depth_i-1]
-    #print(lca)
-    return lca
-
-    
-def first_nonzero(lst):
-    for num in lst:
-        if num != 0:
-            return num
-    return None  # In case all elements are zero
-
-
-def first_nonzero_idx(lst):
-    for idx, num in enumerate(lst):
-        if num != 0:
-            return idx
-    return None  # In case all elements are zero    
-
-def get_tax_index(tree_file, tax_genome_file):
+def get_tax_info(tree_file,tax_genome_file):
     print("Tree file: ", tree_file)
     tree_df = pd.read_csv(tree_file, sep="\t", header=None)
+
     print("Read the tree file") 
     children = tree_df[0]
     parents = tree_df[2]
     info, Tree = parse_tree_file(tree_df,children,parents)
     print("Parsed the tree file. Size of info: ", len(info),"Size of Tree: ", len(Tree))
+
 
     tax_genome_f = open(tax_genome_file,'r') # genomes in the kraken index  
     tax_genome = set()
@@ -109,15 +59,14 @@ def get_tax_index(tree_file, tax_genome_file):
     tax_index =set(tax_index_) # all the tax in genomes of kraken, plus all up to root 
     print('Number of tax ids',len(tax_index),len(tax_index_)) # 
     print("Genome tax not found in kraken tree",tax_notfound)
-    return tax_index
+
+    return info, Tree, tax_index, tax_genome, parents
 
 
+def get_tax2path(tax_genome, info, parents):
 
-def get_tax2root_all(info, parents, tax_genome):
     tax2root_all=[]
-    tax2root_all_dic={}
-    # paths endint at all levels 
-    #tax_index_list= list(tax_index) 
+    tax2root_all_dic={} # paths endint at all levels 
     # paths endint at leaf level (no Vague positive )
     tax_index_list= list(tax_genome)  # tax_genome_specieslevel 
     for tax in tax_index_list:   
@@ -128,11 +77,6 @@ def get_tax2root_all(info, parents, tax_genome):
             tax2root_all.append(tax2root) # # [655353, 655352, 655351, 356, 28211, 1224, 3379134, 2, 131567, 1]
             tax2root_all_dic[tax]=tax2root
     print("number of paths",len(tax2root_all))
-    return tax2root_all, tax2root_all_dic
-
-
-def get_tax2path_all(tax2root_all):
-        # the following is for all tax at all levels
     tax2path = {} # a tax is present in which paths
     tax2depth = {} 
     for tax2root_path_idx, tax2root_path in enumerate(tax2root_all):
@@ -143,30 +87,5 @@ def get_tax2path_all(tax2root_all):
             else:
                 tax2path[tax]=[tax2root_path_idx]
     print(len(tax2path),len(tax2depth))
+
     return tax2path, tax2depth
-
-
-
-
-def get_read_tax_depth(merged, info, parents, cases):
-        
-    read_tax_depth={}
-    for case in cases: #'k19'
-        read_tax_depth[case]={}
-        tocheck_=""
-        for index, row in merged.iterrows():
-            taxid=row['taxon_tool_'+case]
-            tax2root_list = find_tax2root(info,parents, taxid) # [2711156, 1649486, 41297, 204457, 28211, 1224, 3379134, 2, 131567, 1]
-            if tax2root_list!=-1:
-                depth=len(tax2root_list)
-            else:
-                depth=0
-            if depth<3:
-                tocheck_=taxid
-            read_tax_depth[case][row['read_name']]=depth
-            
-    print(tocheck_)
-    print(len(read_tax_depth),len(read_tax_depth[case]))
-    #Counter(read_tax_depth[case].values())
-    return read_tax_depth
-    
