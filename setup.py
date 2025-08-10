597c4e00e7915b5f31ac80e91a49ff96550668de
Sina Majidian <smajidi5@login01>
2025-08-09 20:05:14 -0400

    add a drafty python setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'metakpick',
+  packages = ['metakpick'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'The toolkits to recover the IG region from LCL dataset.',
+  author = 'Sina Majidian',
+  author_email = 'sina.majidian@gmail.com',
+  url = 'https://github.com/sinamajidian/MetaKPick',
+  download_url = 'https://github.com/sinamajidian/MetaKPick/tarball/master',
+  keywords = ['metagenomics', 'classification', 'machine learning', 'random forest'],
+  install_requires=[
+          'numpy',
+          'pysam',
+          'pandas',
+          'matplotlib',
+          'seaborn',
+          'scikit-learn',
+      ],
+  include_package_data=True,
+  #package_data={'metakpick': ['files/*', 'files/nodes.dmp', 'files/seqid2taxid.map_tax_uniq']},
+  zip_safe = False,
+  classifiers=[
+    'Programming Language :: Python :: 3',
+  ],
+  entry_points={"console_scripts": ["metakpick = metakpick.metakpick:main"],},
+)
